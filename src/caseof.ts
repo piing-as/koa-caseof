e23323382ff646deef0a5260b223dc31d31c291c
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2019-05-24 14:15:54 +0200

    export interface

--- src/caseof.ts
+++ src/caseof.ts
@@ -2,15 +2,14 @@ import * as koa from "koa";
 import getAtPath from "lodash.get"
 
 /*
- * A koa Middleware that acts like a switch, taking a path and finding that value on the ctx:IRequestContext
+ * A koa Middleware that acts like a switch, taking a path and finding that value on the ctx:koa.Context
  * Then using the coresponding Middleware in the map of cases.
 */
 
-interface IMiddlewareCases<IContext extends koa.Context> {
+export interface IMiddlewareCases<IContext extends koa.Context> {
 	[key: string]: koa.Middleware<any, IContext>;
 }
 
-// tslint:disable-next-line
 export default <IContext extends koa.Context = koa.Context> 
 	(on: string, cases: IMiddlewareCases<IContext>): koa.Middleware<any, IContext> => async(ctx: IContext, next) => {
 		const key: string = getAtPath(ctx, on) as string;
